a48252e0ba919a8d7186e2c7176115ca97d5df37
gpffh20 <eunbin584836@gmail.com>
2024-04-08 21:38:05 +0900

    initial commit

---
+++ practice/blog/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from django.core.paginator import Paginator
+from .models import Blog
+from .forms import BlogForm
+
+
+def home(request):
+    blogs = Blog.objects.all()
+    paginator = Paginator(blogs, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'home.html',{'page_obj':page_obj})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request,'detail.html',{'blog':blog})
+
+def new(request):
+    return render(request,'new.html')
+
+def create(request):
+    new_blog = Blog()
+    new_blog.title = request.POST.get('title')
+    new_blog.content = request.POST.get('content')
+    new_blog.image = request.FILES.get('image')
+    new_blog.save()
+    return redirect('detail', new_blog.id)
+
+def edit(request, blog_id):
+    edit_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'edit.html', {'edit_blog':edit_blog})
+
+
+def update(request, blog_id):
+    old_blog = get_object_or_404(Blog, pk=blog_id)
+    old_blog.title = request.POST.get('title')
+    old_blog.content = request.POST.get('content')
+    old_blog.image = request.FILES.get('image')
+    old_blog.save()
+    return redirect('detail', old_blog.id)
+
+    if form.is_valid():
+        new_blog = form.save(commit=False)
+        new_blog.save()
+        return redirect('detail', old_blog.id)
+
+    return render(request, 'new.html', {'old_blog':old_blog})
+
+
+def delete(request, blog_id):
+    delete_blog = get_object_or_404(Blog, pk=blog_id)
+    delete_blog.delete()
+    return redirect('home')
